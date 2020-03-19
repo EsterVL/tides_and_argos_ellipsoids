314e0d5a4274fab3744ceeba85aa7b3f4d660315
Ester <61682929+EsterVL@users.noreply.github.com>
2020-03-19 22:10:48 +0100

    Create add_stamps.R

---
+++ add_stamps.R
@@ -0,0 +1,13 @@
+##this script gives all the tables a propper timestamp and locationstamp
+
+#Give all tables a propper timestamp
+tides_table$timestamp <- strptime(tides_table$time, format='%m.%d.%Y %H:%M', tz = 'UTC')
+birdlocation_table$timestamp <- strptime(birdlocation_table$timestamp, format='%Y-%m-%d %H:%M', tz = 'UTC')
+metadata_images_table$timestamp <- strptime(metadata_images_table$timestamp, format='%Y-%m-%d-%H-%M', tz = 'UTC')
+
+#Give the birds a proper locationstamp
+positievogel <- SpatialPointsDataFrame(data.frame(birdlocation_table$location.long, birdlocation_table$location.lat), data.frame(birdlocation_table))
+proj4string(positievogel)=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+
+
+
