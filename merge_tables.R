e1714a6c0e4922eadd45585f33a07199b8bd4ebc
Ester <61682929+EsterVL@users.noreply.github.com>
2020-03-19 22:11:10 +0100

    Create merge_tables.R

---
+++ merge_tables.R
@@ -0,0 +1,8 @@
+##this script will merge all the useful information from the tides table to the birdlocation and the
+##metadata image table
+
+#Merge the waterheight, tide phase and meters to max from the tides table to the birdlocation table
+birdlocation_table <- merge(x=birdlocation_table, y=tides_table, by="timestamp", all.x=T, sort=F)
+
+#Merge the waterheight, tide phase and meters to max from the tides table to the metadata images table
+metadata_images_table <- merge(x=metadata_images_table, y=tides_table, by="timestamp", all.x=T, sort=F)
