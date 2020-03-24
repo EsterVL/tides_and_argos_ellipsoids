ca961c658f3091b831ba46011faaeaccbc434b20
Ester <61682929+EsterVL@users.noreply.github.com>
2020-03-24 12:48:10 +0100

    Update Model_bship.R

--- Model_bship.R
+++ Model_bship.R
@@ -10,14 +10,19 @@ library(SpaDES)
 library(parallel)
 library(snow)
 library(rgdal)
+library(lubridate)
 source('split_raster.R')
 source('cross_gam_function.R')
+
 
 #The tables nessecary to run the model
 tides_table <- read.csv("tables/oman_tides_2016_2020.txt", sep="")
 birdlocation_table <- read.csv("tables/BarWitsOman_Winter2016.csv")
 metadata_images_table <- read.csv("tables/datetime_table.txt", sep="")
 metadata_tiff_table <- list.files(path = "tiff_images", pattern='local_tidal_level_*', full.names=TRUE)
+checkpoint <- read.csv("tables/Stat341.csv")
+loc <- read.csv("tables/station_field_data.csv")
+tides_table_2011 <- read.csv("tables/oman_tides_2011.txt", sep = "")
 
 #This script edits tides_table in a way that all the useful information is available
 source("edit_tides_table.R")
@@ -30,6 +35,9 @@ source("merge_tables.R")
 
 #This script creates two brickfiles from all the tiff images. One brickfile is for ebb and one is for flood.
 source("create_brickfiles.R")
+#if brickfiles already exist, use this code to save time:
+# Flood_raw<-brick('Flood_raw_Oman.grd')
+# Ebb_raw<-brick('Ebb_raw_Oman.grd')
 
 #This script creates two new tables. One table with the information about the birds that are present during ebb
 #and one table with the information about the birds that are present during flood. Both tables also have an
@@ -41,10 +49,10 @@ source("edit_bird_table.R")
 #source("dummybird.R")
 #This script uses a very accurate measuring instrument to check how accurate the model is with predicting
 #the presence of water and land on a position.
-#NOT READY YET
+#source("check_accuracy.R")
 
 #this script creates useful craphs from the flow birds table and the ebb birds table
-source("creating_graphs")
+source("creating_graphs.R")
 
 
 
