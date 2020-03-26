cceea3e19180f655e32d73b0b3d06cefc04c7127
Ester <61682929+EsterVL@users.noreply.github.com>
2020-03-26 16:46:33 +0100

    Update check_accuracy.R

--- check_accuracy.R
+++ check_accuracy.R
@@ -1,55 +1,32 @@
 ##editing the time of the checkpoint and give timestamps to checkpoint and tides table 2011
-checkpoint$dateTimeOman <- as.POSIXct(checkpoint$Date.......Time, format='%d %m %y %H:%M', tz = 'Asia/Dubai')
+checkpoint$dateTimeOman <- as.POSIXct(checkpoint[, c(1)], format='%d %m %y %H:%M', tz = 'Asia/Dubai')
 checkpoint$timestamp <- as.POSIXct(format(checkpoint$dateTimeOman,tz="UTC"), format='%Y-%m-%d %H:%M:%S')
 tides_table_2011$timestamp <- paste(tides_table_2011$mm.dd.yyyy, tides_table_2011$hh.mm.ss, sep = ' ')
 tides_table_2011$timestamp <- as.POSIXct(strptime(tides_table_2011$time, format='%m.%d.%Y %H:%M', tz = 'UTC'), format='%Y-%m-%d %H:%M:%S')
 
 
 ##The location of the measuring instrument
-loc.sp <- SpatialPointsDataFrame(data.frame(loc$X[which(loc$ID == 341)], loc$Y[which(loc$ID == 341)]), data.frame(loc[which(loc$ID == 341), ]))
+loc.sp <- SpatialPointsDataFrame(data.frame(loc$X[which(loc$ID == number)], loc$Y[which(loc$ID == number)]), data.frame(loc[which(loc$ID == number), ]))
 proj4string(loc.sp)=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
 flood_value <- as.numeric(extract(Flood_raw,loc.sp[,c(1,2)], cellnumbers=F))
 ebb_value <- as.numeric(extract(Ebb_raw,loc.sp[,c(1,2)], cellnumbers=F))
 
-##get a meters to max in tides_table 2011
-tides_table_2011$meters_to_max <- as.numeric(max(tides_table_2011$z.m.) - tides_table_2011$z.m.)
-tides_table_2011$tide_next_point <- shift(tides_table_2011$z.m., n=1, fill=NA, type="lead")
-tides_table_2011$tide_phase<-ifelse(tides_table_2011$tide_next_point > tides_table_2011$z.m.,"flow","ebb")
-tides_table_2011<-tides_table_2011[!is.na(tides_table_2011$tide_phase),]
-tides_table_2011$meters_to_max[tides_table_2011$tide_phase=='flow']<- -tides_table_2011$meters_to_max[tides_table_2011$tide_phase=='flow']
-
-
-##select the year 2016 and the date times of the tides table that are equal to those of the checkpoint table
-tt <- tides_table_2011[tides_table_2011$timestamp >= "2011-12-02 11:39:00" & tides_table_2011$timestamp < "2011-12-03 09:00:00",]
-
-  
-##select the hours and minutes of both tables, to be able to properly merge them
-tt$tijd <- paste(as.numeric(format(tt$timestamp, '%H')), as.numeric(format(tt$timestamp, '%M')), sep = ':')
-checkpoint$tijd <- paste(as.numeric(format(checkpoint$timestamp, '%H')), as.numeric(format(checkpoint$timestamp, '%M')), sep = ':')
+##get a meters to max and tide phase in tides_table 2011
+tides_table_2011$meters_to_max <- as.numeric(max(tides_table$z.m.) - tides_table_2011$z.m.)
+tides_table_2011$tide_phase<-ifelse(tides_table_2011$z.m. < 0,"flow","ebb")
 
 ##merge the checkpoint table and the subsection of the tides table to gather all the data in one table
-df <- merge(x=checkpoint, y=tt, by="tijd", all.X=T, sort=F)
+df <- merge(x=checkpoint, y=tides_table_2011, by="timestamp", all.X=T, sort=F)
+df$next_depth <- shift(df[, c(3)], n=1, fill=NA, type="lead")
 
-##Get the rows of the tides table that represent the time of the ebb and flood values.
-ebb_lijn <- df[which(abs(df$meters_to_max-ebb_value)==min(abs(df$meters_to_max-ebb_value))), ]
-vloed_lijn <- df[which(abs(df$meters_to_max-flood_value)==min(abs(df$meters_to_max-flood_value))), ]
+#find the average meters to max value where the measuring instrument detects zero water height.
+zero_vector <- cbind(df$meters_to_max[which(df[, c(3)]!=0.00 & df$next_depth == 0.00)], df$meters_to_max[which(df[, c(3)]==0.00 & df$next_depth != 0.00)])
+zero_line <- sum(zero_vector)/length(zero_vector)
 
-##plot the waterhight from the measuring instrument over time
-plot(checkpoint$Depth341 ~ as.POSIXct(x = checkpoint$timestamp, format = "%H"), type = 'o', pch = 21, bg = 'orange',
-     xlab = 'time', ylab = 'water depth (m)', main = '2011')
-
-abline(v=ebb_lijn$timestamp.x, col = 'red')
-abline(v=vloed_lijn$timestamp.x, col = 'blue')
-
-# timedifference between model and measuring instrument for ebb
-# model -> 12:39:00
-# meetinstrument -> 14:49:00
-# 
-# timedifference between model and measuring instrument for flood
-# model -> 19:19:00
-# meetinstrument -> 20:49:00
-
-
-
-
+#plot the meters to max of df with the zero water height of the measuring instrument and the ebb and flood values of the images.
+plot(df[, c(11)] ~ as.POSIXct(x = df$timestamp, format = "%H"), type = 'o', pch = 21, bg = 'orange',
+           xlab = 'time', ylab = 'meters to max (m)', main = number)
+abline(h=zero_line, col = 'black')
+abline(h=ebb_value, col = 'red')
+abline(h=abs(flood_value), col = 'blue')
 
