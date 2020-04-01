75bf320784c8526b368e326512417eeb672de36b
Ester <61682929+EsterVL@users.noreply.github.com>
2020-04-01 11:11:48 +0200

    Update creating_graphs.R

--- creating_graphs.R
+++ creating_graphs.R
@@ -1,18 +1,27 @@
+#This script is called upon by the script "Model_Bship.R"
+
 #renaming the tables and editing the enviroment collumns to be able to create plots.
 ebb <- Ebb_birds_table
 flood <- Flow_birds_table
 ebb$enviroment <- as.factor(ebb$enviroment)
 flood$enviroment <- as.factor(flood$enviroment)
 
-#creating two boxplots 
-par(mfrow=c(1,2))
-plot(ebb$enviroment, ebb$meters_to_max, ylab="meters to max", xlab = "enviroment", main= "boxplot ebb")
-plot(flood$enviroment, flood$meters_to_max, ylab="meters to max", xlab = "enviroment", main= "boxplot flood")
 
-#show the percentages of birds in the water vs on land when the meters to max is devided in 10 categories
-mtm_ebb <- cut(ebb$meters_to_max, 10, labels=c(1:10))
-mtm_flood <- cut(flood$meters_to_max, 10, labels=c(1:10))
-plot(mtm_ebb, ebb$enviroment)
-plot(mtm_flood, flood$enviroment)
+
+#creating two boxplots that show the distribution of birds along the waterline
+par(mfrow=c(1,2))
+e_to_waterline <- Ebb_birds_table$tiff_value - Ebb_birds_table$meters_to_max
+boxplot(e_to_waterline, ylab="water height (in meters)", xlab = "birds", main= "Ebb")
+abline(h=0, col="blue")
+
+f_to_waterline <- abs(Flow_birds_table$tiff_value) - abs(Flow_birds_table$meters_to_max)
+boxplot(f_to_waterline, ylab="water height (in meters)", xlab = "birds", main= "Flood")
+abline(h=0, col="blue")
+
+
+#show the number of birds on land and in the water for ebb and flood
+plot(ebb$enviroment, ylab="number of birds", main= "Ebb")
+plot(flood$enviroment, ylab="number of birds", main= "Flood")
+
 
 
