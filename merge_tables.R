e9df98bee18c19406f81c13fd69a9ef52547b569
Ester <61682929+EsterVL@users.noreply.github.com>
2020-04-01 11:14:09 +0200

    Update merge_tables.R

--- merge_tables.R
+++ merge_tables.R
@@ -1,3 +1,5 @@
+#This script is called upon by the script "Model_Bship.R"
+
 ##this script will merge all the useful information from the tides table to the birdlocation and the
 ##metadata image table
 
